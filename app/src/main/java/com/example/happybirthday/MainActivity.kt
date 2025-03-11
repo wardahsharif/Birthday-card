47c1e8cb7f97eb70aa2a4331995c643004bca5d3
Warda Sharif Hassan <warda_sharif@otmail.com>
2025-03-12 02:02:52 +0300

    everythng u to date

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -74,7 +74,8 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
             fontSize = 36.sp,
              modifier = Modifier
                  .padding(20.dp)
-                 .align(alignment = Alignment.End)
+                 .align(alignment = Alignment.CenterHorizontally
+                 )
         )
     }
 }
